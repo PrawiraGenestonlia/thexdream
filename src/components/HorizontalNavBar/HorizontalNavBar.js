0d3a38ab512818cf9b1dc44596d27451be191117
PrawiraGenestonlia <prawira.96@gmail.com>
2019-08-06 00:35:12 +0800

    Fast Commit

--- src/components/HorizontalNavBar/HorizontalNavBar.js
+++ src/components/HorizontalNavBar/HorizontalNavBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Logo from '../../assets/logo/TheXDreamLogo_DarkBGS.png';
+// import Logo from '../../assets/logo/TheXDreamLogo_DarkBGS.png';
 
 export default function HorizontalNavBar() {
   return (
     <div style={{ padding: '10px' }}>
       <a href='/page/coming-soon'>
-        <img src={Logo} alt="Logo" style={{ height: '40px' }} />
+        {/* <img src={Logo} alt="Logo" style={{ height: '40px' }} /> */}
       </a>
 
     </div>
