7a9d9b67b80ed4b03e858e63537794849f9ec924
PrawiraGenestonlia <prawira.96@gmail.com>
2019-08-06 00:31:55 +0800

    Third Commit

--- src/routes/page/error-404/Error404.js
+++ src/routes/page/error-404/Error404.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { LightBulb } from './../../../assets';
+// import { LightBulb } from './../../../assets';
 import './Error404.scss';
 
 export default function fourzerofour() {
   return (
     <div className="errorpage" style={{ backgroundColor: "black" }}>
       <body>
-        <img src={LightBulb} alt="light-bulb" class="image" />
+        {/* <img src={LightBulb} alt="light-bulb" class="image" /> */}
         <div className="dialog">
           <h1>Hey, who turned off the lights?</h1>
           <p>We were unable to find the page you were looking for.</p>
